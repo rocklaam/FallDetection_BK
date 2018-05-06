1c0ffaf504911454ae34483fdbc2f1d8da49d384
rocklaam <wildboar.work@gmail.com>
2018-05-06 08:53:16 +0700

    Continue Completing Core:     + fix theLargestArea to prevent calculating when rotating camera     + Cmotion threshold changed to 0.9     + Applied 2nd condition after 1stNum checked     - need to apply final condition (unmove condition, duration 0.5- 1s)

--- FallDetection_Core/MHIProcess.cpp
+++ FallDetection_Core/MHIProcess.cpp
@@ -1,12 +1,12 @@
 #include "MHIProcess.h"
 
 //Properties
-vector<Mat> MHIProcess::buf = vector<Mat>(2);
+vector<Mat> MHIProcess::_buf = vector<Mat>(2);
 int MHIProcess::_last = 0;
 Mat MHIProcess::_mhi = Mat();
 
 //Methods
-void MHIProcess::updateMHI(const Mat& img, Mat& dst, int diff_threshold, double& sumHist){
+void MHIProcess::updateMHI(const Mat& img, Mat& dst, int diff_threshold, float& sumHist){
 	double timestamp = (double)clock() / CLOCKS_PER_SEC; // get current time in seconds
 	Size size = img.size();
 	int idx1 = _last;
@@ -15,26 +15,24 @@ void MHIProcess::updateMHI(const Mat& img, Mat& dst, int diff_threshold, double&
 	if (_mhi.size() != size)
 	{
 		_mhi = Mat::zeros(size, CV_32F);
-		buf[0] = Mat::zeros(size, CV_8U);
-		buf[1] = Mat::zeros(size, CV_8U);
+		_buf[0] = Mat::zeros(size, CV_8U);
+		_buf[1] = Mat::zeros(size, CV_8U);
 	}
 
-	cvtColor(img, buf[_last], COLOR_BGR2GRAY); // convert frame to grayscale
+	cvtColor(img, _buf[_last], COLOR_BGR2GRAY); // convert frame to grayscale
 
 	int idx2 = (_last + 1) % 2; // index of (_last - (N-1))th frame
 	_last = idx2;
 
-	Mat silh = buf[idx2];
-	absdiff(buf[idx1], buf[idx2], silh); // get difference between frames
+	Mat silh = _buf[idx2];
+	absdiff(_buf[idx1], _buf[idx2], silh); // get difference between frames
 
 	threshold(silh, silh, diff_threshold, 1, THRESH_BINARY); // and threshold it
 	updateMotionHistory(silh, _mhi, timestamp, MHI_DURATION); // update MHI
 	_mhi.convertTo(dst, CV_8U, 255. / MHI_DURATION, (MHI_DURATION - timestamp)*255. / MHI_DURATION);// convert MHI to 8u image
-
-	// calculate sumHist
-	for (int i = 0; i < dst.rows; i++){
-		for (int j = 0; j < dst.cols; j++){
-			sumHist += dst.at<uchar>(i, j);
-		}
-	}
+	Mat1f bufVect;
+	reduce(dst, bufVect, 0, CV_REDUCE_SUM);
+	reduce(bufVect, bufVect, 1, CV_REDUCE_SUM);
+	bufVect /= 255;
+	sumHist = bufVect.at<float>(0, 0);
 }
