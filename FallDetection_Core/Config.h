7f611ac87ea4babf9248f1487dda72dba13fe8fd
rocklaam <wildboar.work@gmail.com>
2018-05-08 16:21:35 +0700

    Continue Completing Core: + applied final condition (unmove condition, duration 1s) + add a curl method to push notification

--- FallDetection_Core/Config.h
+++ FallDetection_Core/Config.h
@@ -1,22 +1,25 @@
 // top limit of human rect- to prevent detecting fall in rotate camera
-#define AREA_MAX			28800
+#define AREA_MAX				28800
 
 // duration of stage- (unit: seconds)
-#define DURATION_D0			1
-#define DURATION_DY			2
+#define DURATION_D0				1
+#define DURATION_DY				1
 
-#define FRAME_HEIGHT		180
-#define FRAME_WIDTH			320
+#define FRAME_HEIGHT			180
+#define FRAME_WIDTH				320
 
 // duration of MHI- (unit: seconds)
-#define MHI_DURATION		0.5
+#define MHI_DURATION			0.5
 
 // stage of algorithm- for switch- case
-#define STAGE_CMOTION		1
-#define STAGE_D0			2
-#define STAGE_DY			3
+#define STAGE_CMOTION			1
+#define STAGE_D0				2
+#define STAGE_DY				3
 
 // threshold of algorithm
-#define THRESHOLD_CMOTION	0.9 //1.5
-#define THRESHOLD_D0		25
-#define THRESHOLD_DA_B		0.9
+#define THRESHOLD_CMOTION		1.2 //0.9
+#define THRESHOLD_CMOTION_DY	0.15
+#define THRESHOLD_DA_B			0.9
+#define THRESHOLD_D0			35
+#define THRESHOLD_DX			2
+#define THRESHOLD_DY			2
